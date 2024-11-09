3bb7d32adf74668ea6f3606fc79a1598d6edb24f
Richard Chadwick <richardchadp@gmail.com>
2024-11-09 21:36:04 +0100

    Initial commit - added all project files

---
+++ data_types/variable_types.py
@@ -0,0 +1,20 @@
+# variable_types.py
+# Richard Chadwick Plaza
+# This program demonstrates the defenition of different numerical data and types
+
+# Integer
+integerValue = 10
+print("Integer value: ", integerValue, "| Type: ", type(integerValue))
+
+# Float
+floatValue = 10.5
+print("Float value: ", floatValue, "| Type: ", type(floatValue))
+
+# Boolean
+booleanTrue = True
+booleanFalse = False
+print("Boolean True", booleanTrue, "| Type:", type(booleanTrue))
+print("Boolean False: ", booleanFalse, "| Type: ", type(booleanFalse))
+
+# String
+stringValue = 
