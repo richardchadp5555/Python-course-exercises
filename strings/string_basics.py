4cb59309b97695e34fc2392952208b0344c242b6
Richard Chadwick <richardchadp@gmail.com>
2024-11-14 12:55:14 +0100

    Add string_basics.py with basic string operations examples

---
+++ strings/string_basics.py
@@ -0,0 +1,13 @@
+# string_basics.py
+# Demosntrates basic string operations in Python
+
+greeting = "Hello"
+name = "Python"
+
+# Concatenation
+message = greeting + " " + name
+print("Concatenated message " + message)
+
+# Repetition 
+repeatedMessage = (greeting + " ") * 3
+print("Repeating the message: " + repeatedMessage)
